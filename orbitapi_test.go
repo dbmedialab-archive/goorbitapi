54bc53bfea439d09c3f16675ff9ec4f8e3391cff
JustAdam <adambell7@gmail.com>
2014-06-16 19:23:37 +0200

    Added new tests that the API key is sent correctly to Orbit.  Added test for accountinfo method.

--- orbitapi_test.go
+++ orbitapi_test.go
@@ -6,9 +6,20 @@ package orbitapi
 
 import (
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
 	"os"
 	"testing"
 )
+
+type TestData struct {
+	n string
+	v interface{}
+	e interface{}
+}
 
 func TestOrbitTagDecode(t *testing.T) {
 	td, err := os.Open("test_data/tag.json")
@@ -22,11 +33,7 @@ func TestOrbitTagDecode(t *testing.T) {
 		t.Error("Failed decoding json data:", err)
 	}
 
-	testData := []struct {
-		n string
-		v interface{}
-		e interface{}
-	}{
+	testData := []TestData{
 		{"Entities[\"Erna_Solberg\"].Image", data.Entities["Erna_Solberg"].Image, "http://upload.wikimedia.org/wikipedia/commons/2/25/Erna_Solberg,_Wesenberg,_2011_(1).jpg"},
 		{"Entities[\"Erna_Solberg\"].Label", data.Entities["Erna_Solberg"].Label, "Erna Solberg"},
 		{"Entities[\"Erna_Solberg\"].Link", data.Entities["Erna_Solberg"].Link, "http://no.wikipedia.org/wiki/Erna_Solberg"},
@@ -53,4 +60,86 @@ func TestOrbitTagDecode(t *testing.T) {
 			t.Errorf("%v: expecting %v, got %v", d.n, d.e, d.v)
 		}
 	}
+}
+
+func TestAccountInfoRequest(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		o, err := ioutil.ReadFile("test_data/info.json")
+		if err != nil {
+			t.Fatal("Unable to open info test data file")
+		}
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprintln(w, string(o))
+	}))
+	defer ts.Close()
+
+	orbitApiUrl = ts.URL + "/"
+	api := NewClient("apiKey")
+	go func() {
+		if err := api.AccountInfo(); err != nil {
+			t.Fatal(err)
+		}
+	}()
+
+	d := <-api.Result
+	data := d.(map[string]interface{})
+	testData := []TestData{
+		{"iptc_requests_limit", data["iptc_requests_limit"], float64(150)},
+		{"name", data["name"], "JustAdam"},
+		{"concurrent_requests", data["concurrent_requests"], nil},
+		{"iptc_requests_today", data["iptc_requests_today"], float64(0)},
+		{"daily_word_limit", data["daily_word_limit"], float64(10000)},
+		{"langdetect_requests_limit", data["langdetect_requests_limit"], float64(150)},
+		{"langdetect_requests_today", data["langdetect_requests_today"], float64(0)},
+		{"words_today", data["words_today"], float64(8)},
+		{"api_key", data["api_key"], "XXX"},
+		{"id", data["id"], float64(1)},
+	}
+
+	for _, d := range testData {
+		if d.v != d.e {
+			t.Errorf("%v: expecting %v, got %v", d.n, d.e, d.v)
+		}
+	}
+}
+
+func TestGetRequestAPIKeyIsSet(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Header["X-Orbit-Api-Key"][0] != "apiKey" {
+			t.Errorf("Expecting 'apiKey', got %v", r.Header["X-Orbit-Api-Key"][0])
+		}
+	}))
+	defer ts.Close()
+
+	orbitApiUrl = ts.URL
+	api := NewClient("apiKey")
+	api.Get("")
+}
+
+func TestPostRequestContentType(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Header["Content-Type"][0] != "application/x-www-form-urlencoded" {
+			t.Errorf("Expecting 'application/x-www-form-urlencoded', got %v", r.Header["Content-Type"][0])
+		}
+	}))
+	defer ts.Close()
+
+	orbitApiUrl = ts.URL
+	api := NewClient("apiKey")
+	api.Post("", url.Values{})
+}
+
+func TestPostRequestAPIKeyIsSet(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		body, _ := ioutil.ReadAll(r.Body)
+		v, _ := url.ParseQuery(string(body))
+		if v["api_key"][0] != "apiKey" {
+			t.Errorf("Expecting 'apiKey', got %v", v["api_key"][0])
+		}
+	}))
+	defer ts.Close()
+
+	orbitApiUrl = ts.URL
+	api := NewClient("apiKey")
+	api.Post("", url.Values{})
 }

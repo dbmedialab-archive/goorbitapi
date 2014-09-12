05a44650682dc8fd4a4b6008ee8b18856db7e179
AdamB <abe@dagbladet.no>
2014-09-12 10:17:33 +0200

    Additional sample code: textquery - fetch information about each entity from wikipedia.

--- README.md
+++ README.md
@@ -5,6 +5,22 @@ GO OrbitAPI client
 
 Go client for accessing the Orbit API - http://orbitapi.com/
 
-See the *cmd/* directory for sample code.
-
 Currently only Concept Tagging call has its own data type and API call.  All other calls can be made against the API, but these need to be done through Post() or Get().
+
+See the `cmd/` directory for sample code:
+
+## Quick start
+
+```
+$ cd cmd
+$ vim sample.php
+	Edit line 11 and add your API key
+$ go run sample.php
+```
+
+### Text query: entity information from Wikipedia
+
+```
+$ cd cmd
+$ ORBIT_API_KEY=YOUR-KEY go run textquery.go -text "Jeg liker politikk sa Solberg til Dagbladet." -sentences 3
+```
---
+++ cmd/textquery.go
@@ -0,0 +1,94 @@
+// Copyright 2014 DB Medialab.  All rights reserved.
+// License: MIT
+
+// $ ORBIT_API_KEY=YOUR-KEY go run textquery.go -text "Jeg liker politikk sa Solberg til Dagbladet." -sentences 3
+package main
+
+import (
+  "fmt"
+  "github.com/dbmedialab/goorbitapi"
+  "log"
+  "net/url"
+  "net/http"
+  "flag"
+  "encoding/json"
+  "os"
+)
+
+var (
+  apiKey string
+  text string
+  sentences int
+)
+
+func init() {
+  apiKey = os.Getenv("ORBIT_API_KEY")
+  flag.StringVar(&text, "text", "", "Text to use in your query")
+  flag.IntVar(&sentences, "sentences", 1, "Number of sentences to extract from Wikipedia")
+}
+
+func main() {
+  flag.Parse()
+
+  if apiKey == "" || text == "" {
+    flag.Usage()
+    return
+  }
+
+  api := orbitapi.NewClient(apiKey)
+
+  tag := make(chan *orbitapi.OrbitTag, 1)
+  go func() {
+    args := &url.Values{}
+    args.Add("text", text)
+    if err := api.ConceptTag(tag, args); err != nil {
+      log.Fatal(err)
+    }
+  }()
+
+  t := <-tag
+  if len(t.Entities) < 1 {
+    fmt.Println("Nothing!")
+    return
+  }
+  
+  fmt.Printf("What we think we know about that:\n")
+
+  desc := make(chan string, len(t.Entities))
+  for _, t := range t.Text[0].([]interface{}) {
+    if item, ok := t.(map[string]interface{}); ok {
+      go queryWiki(desc, item)
+    }
+  }
+
+  for i := 0; i < len(t.Entities); i++{
+    fmt.Printf(<-desc)
+  }
+  fmt.Println()
+}
+
+func queryWiki(desc chan<- string, item map[string]interface{}) {
+  url:=fmt.Sprintf("http://no.wikipedia.org/w/api.php?format=json&action=query&titles=%s&prop=extracts&exsentences=%d&explaintext", item["entity"].(string), sentences)
+  resp, err := http.Get(url)
+  defer resp.Body.Close()
+  if err != nil {
+    log.Fatal(err)
+  }
+  var data map[string]interface{}
+  err = json.NewDecoder(resp.Body).Decode(&data)
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  var r string
+  // Our data is held in an unknown key
+  for _,v := range data["query"].(map[string]interface{})["pages"].(map[string]interface{}) {
+    if e, ok := v.(map[string]interface{})["extract"].(string); ok {
+      r = e
+    } else {
+      r = "Nothing yet!"
+    }
+    break
+  }
+  desc <- fmt.Sprintf("\n%s\n\t%s\n", item["text"], r)
+}
